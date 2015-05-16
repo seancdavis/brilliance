a39960f08477a791c5de1d3a25f64b6725739e93
Sean C Davis <scdavis41@gmail.com>
2015-05-16 07:32:16 -0400

    init commit with cambium

---
+++ config/initializers/cambium.rb
@@ -0,0 +1,16 @@
+Cambium.configure do |config|
+
+  # -------------------------------------------------- App Info
+
+  # Cambium uses these to set your root urls for
+  # mailers and other fun stuff.
+  #
+  config.development_url = 'localhost:3000'
+  config.production_url = 'example.com'
+
+  # The title of your app is what Cambium puts in the
+  # browser tab by default.
+  #
+  config.app_title = 'Brilliance'
+
+end
